02114caee17d3c7516db38f26525e039b3e2952e
tom.pedron <tom.pedron@digitalalchemy.global>
2020-09-28 17:51:43 -0400

    add output for mediator app

--- behavioral_patterns/mediator/application.rb
+++ behavioral_patterns/mediator/application.rb
@@ -17,8 +17,20 @@ carl.sale_offer("NRG", 10)
 
 tom.buy_offer("NRG", 10)
 
-puts "STOCK FOR SALE"
+puts "STOCKS FOR SALE"
 mediator.sell_offers.each do |stock|
     puts "#{stock.symbol} - #{stock.num_shares}"
 end
 
+# OUTPUT
+# TOM HAS SIGNED UP FOR EXCHANGE
+# EXCHANGE RECEIVED REGISTRATION OF COLLEAGUE ["TOM"]
+# CARL HAS SIGNED UP FOR EXCHANGE
+# EXCHANGE RECEIVED REGISTRATION OF COLLEAGUE ["CARL"]
+# 100 shares of MSFT added to inventory
+# 50 shares of GOOG added to inventory
+# 100 shares of MSFT bought by colleague code TOM
+# 10 shares of NRG added to inventory
+# 10 shares of NRG bought by colleague code CARL
+# STOCKS FOR SALE
+# GOOG - 50
